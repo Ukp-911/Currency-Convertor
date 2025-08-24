f295a2894e908b5f6f85e43edb671581e5318b81
Ukp-911 <codingukp@gmail.com>
2025-08-24 15:50:49 +0530

    all files

---
+++ money.js
@@ -0,0 +1,208 @@
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+};
+
+let currcodeL = "inr";
+let concodeL = "IN";
+let currcodeR = "usd";
+let concodeR = "US";
+
+let dropdown = document.querySelectorAll("select");
+for (let i of dropdown) {
+    for (let j in countryList) {
+        let el = document.createElement("option");
+        el.setAttribute("value", countryList[j]);
+        el.textContent = countryList[j];
+        i.append(el);
+    }
+    i.addEventListener("change", (evt) => {
+        let tar = evt.target;
+        if (tar.className == "leftcoun") {
+            concodeL = tar.value;
+            let opt = concodeL;
+            let key = Object.keys(countryList).find(k => countryList[k] === opt);
+            currcodeL = key.toLowerCase();
+        } else {
+            concodeR = tar.value;
+            let opt = concodeR;
+            let key = Object.keys(countryList).find(k => countryList[k] === opt);
+            currcodeR = key.toLowerCase();
+        }
+        flags(evt.target);
+    })
+}
+const flags = (val) => {
+    let imga = val.parentElement.querySelector("img");
+    imga.src = `https://flagsapi.com/${val.value}/flat/64.png`;
+}
+
+const conversion = async () => {
+    let code =  await fetch(`https://2024-03-06.currency-api.pages.dev/v1/currencies/${currcodeR}.json`);
+    let value = await code.json();
+    let inp = document.querySelector("#enter").value;
+    let convert = inp*value[currcodeR][currcodeL];
+    let res=document.querySelector(".result");
+    res.textContent=`${inp} ${currcodeL.toUpperCase()} = ${convert} ${currcodeR.toUpperCase()}`;
+}
+let btn = document.querySelector(".exbtn");
+btn.addEventListener("click", () => {
+    conversion();
+})
